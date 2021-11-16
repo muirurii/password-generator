2b00c76e2204844bfd0c4abfd5d9fa031aa4507f
muirurii <pnkanuna@gmail.com>
2021-11-16 10:30:21 +0300

    Added copy to clipboard functionality

--- pass.js
+++ pass.js
@@ -3,14 +3,17 @@ const allLower = generateElements(97,122);
 const allNums = generateElements(48,57);
 const allSymbols = generateElements(33,47).concat(generateElements(58,64)).concat(generateElements(123,126));
 const form = document.querySelector('form');
+const copyBtn = document.querySelector('#copy');
 const upperElement = document.getElementById('upper');
 const lowerElement = document.getElementById('lower');
 const numberElement = document.getElementById('number');
 const symbolElement = document.getElementById('symbol');
 const inputElement = document.getElementById('length');
 const outputElement = document.getElementById('output');
+const notification = document.querySelector('#notification');
 
 form.addEventListener('submit',generatePassword);
+copyBtn.addEventListener('click',copyFunction);
 
 function generateElements(from,to){
     const allElements = [];
@@ -39,8 +42,19 @@ function generateFromSelection(includeUpper,includeLower,includeNumber,includeSy
         if(includeSymbol) passwordArray.push(...allSymbols);
         
         let password = [];
-        for(let i = 0;i<=length;i++){
+        for(let i = 0;i<length;i++){
             password.push(passwordArray[Math.floor(Math.random() * passwordArray.length)]);
         }
         return password.join('');
+}
+function copyFunction(){
+    if(!outputElement.textContent.length) return '';
+    const textarea = document.createElement('textarea');
+    textarea.value = outputElement.textContent;
+    document.body.append(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    notification.classList.add('show');
+    setTimeout(()=>notification.classList.remove('show'),1500);
 }
